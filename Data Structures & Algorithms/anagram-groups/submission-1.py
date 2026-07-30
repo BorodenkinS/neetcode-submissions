1971375bde76b281a442538946d96fc64c10cab7
Divanniy_analitic <117065250+BorodenkinS@users.noreply.github.com>
2026-07-30 19:58:51 +0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,20 @@
+from array import array
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = dict()
+
+        def countFreq(s):
+            freq = [0 for _ in range(26)]
+            for char in s:
+                freq[ord(char) - ord('a')] += 1
+            return tuple(freq)
+
+        for s in strs:
+            key = countFreq(s)
+            if key in anagrams.keys():
+                anagrams[key].append(s)
+            else:
+                anagrams.update({key: [s]})
+
+        return list(anagrams.values())
